711269807b9109ed1673f7b1555ec56daee2d1ff
seasnail8169 <seasnail8169@gmail.com>
2021-01-18 00:09:53 +0000

    Debug

--- src/main/java/seasnail/api/Main.java
+++ src/main/java/seasnail/api/Main.java
@@ -1,11 +1,10 @@
 package seasnail.api;
 
-import java.util.Map;
-
 public class Main {
   public static void main(String[] args) {
 //    WebServer.init();
-    System.out.println("\nRead All Variables:-\n");
-    for (Map.Entry <String, String> entry: System.getenv().entrySet()) System.out.println(entry.getKey() + ": " + entry.getValue());
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    System.out.println("PORT " + processBuilder.environment().get("PORT"));
+    System.out.println("TOKEN " + processBuilder.environment().get("TOKEN"));
   }
 }
