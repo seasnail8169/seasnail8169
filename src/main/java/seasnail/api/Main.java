939465b890041f2c523dffd1dcc74e546cb63a65
seasnail8169 <seasnail8169@gmail.com>
2021-01-18 00:07:27 +0000

    Debug

--- src/main/java/seasnail/api/Main.java
+++ src/main/java/seasnail/api/Main.java
@@ -1,9 +1,11 @@
 package seasnail.api;
 
-import seasnail.api.webserver.WebServer;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
-    WebServer.init();
+//    WebServer.init();
+    System.out.println("\nRead All Variables:-\n");
+    for (Map.Entry <String, String> entry: System.getenv().entrySet()) System.out.println(entry.getKey() + ": " + entry.getValue());
   }
 }
